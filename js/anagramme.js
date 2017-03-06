fc3c4bdbe2e95d437d70ca7d65b7a2bf8c395282
Nedjim <nedjim.danimon@gmail.com>
2017-03-06 17:25:03 +0100

    Add style + input finish

--- js/anagramme.js
+++ js/anagramme.js
@@ -1,11 +1,19 @@
-let string1 = 'demain';
-let string2 = 'damien';
+let content = document.getElementById('result');
 
-function anagramme(mot1,mot2){
-    var tmp1 = mot1.split("").sort().join();
-    var tmp2 = mot2.split("").sort().join();
+function anagramme(string1,string2){
+    var tmp1 = string1.split("").sort().join();
+    var tmp2 = string2.split("").sort().join();
     return tmp1 == tmp2;
 }
 
-let result = anagramme(string1, string2);
-console.log(result);
+
+document.getElementById('formulaire').addEventListener('submit', function(e){
+    e.preventDefault();
+    let string1 = document.getElementById('string1').value.toLowerCase();
+    let string2 = document.getElementById('string2').value.toLowerCase();
+
+    anagramme(string1, string2) ?  content.innerHTML = "Les deux mots sont des anagrammes" : content.innerHTML = "Les deux mots ne sont pas des anagrammes";
+})
+
+
+
